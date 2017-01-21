4a81d293f638a18aa4bf518030c13c7c762bceb7
Bolza <stefano.sergio@gmail.com>
2017-01-21 11:32:09 +0000

    platform collision detection

---
+++ Assets/Jetroid/Scripts/MoveForward.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour {
+	public float speed = 10f;
+	private Rigidbody2D body;
+
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		body.velocity = new Vector2 (transform.localScale.x, 0) * speed;
+	}
+}
